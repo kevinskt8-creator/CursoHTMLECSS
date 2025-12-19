55881511adebd6b7ac24f19a579f06a5151c9a41
kevinskt8-creator <kevinskt8@gmail.com>
2025-12-19 16:39:41 -0300

    js

---
+++ matutinoatividades/desafio002.js
@@ -0,0 +1,12 @@
+// PI = 3.141592
+// PI * raio * raio
+
+
+const PI = 3.141592;
+const raio = 10;
+let areacCirc = PI * raio * raio;
+
+console.log("O valor da área é " + areacCirc + "m2.")
+
+areacCirc = Math.PI * raio * raio;
+console.log("O valor da área é " + areacCirc + "m2.")
